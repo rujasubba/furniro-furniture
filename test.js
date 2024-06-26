4e84608b528987ad5047fcc827203aef9ae40a1c
RujaSubba <rujasubba123@gmail.com>
2024-06-26 13:32:10 +0930

    Contact page has been initiated and the form submission in local storage is completed.

--- test.js
+++ test.js
@@ -1,9 +1,45 @@
-const users = [
-    { name: "John", age: 25 },
-    { name: "Jane", age: 30 },
-    { name: "Joe", age: 22 }
-];
-// Use map to create a new array with only the names
-const names = users.map(user => user.name);
+// let num = 10
+// while (num < 20){
+//     print (num)
+//     num += 4
 
-console.log(names);
+//     if (num == 18) 
+//         break;
+// }
+
+
+class Person {
+    constructor(name, age, email, fullMarks, passMarks, obtainedMarks, subject) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.subject = subject;
+        this.fullMarks = fullMarks;
+        this.passMarks = passMarks;
+        this.obtainedMarks = obtainedMarks;
+    }
+        isPass() {
+            if (this.obtainedMarks >= 80)
+                console.log(`${this.name} with email address ${this.email} has secured distinction in ${this.subject} with ${this.obtainedMarks} marks.`)
+            else if(this.obtainedMarks >50){
+                console.log(`${this.name} with email address ${this.email} has passed ${this.subject} with ${this.obtainedMarks} marks.`)}
+            else {
+                console.log(`${this.name} has failed the exam.`)
+            }
+
+
+        } 
+    }
+
+
+
+let student = new Person('Dipak', 34, 'knight.rider200072@gmail.com', 100, 50, 60, 'Maths')
+let student1 = new Person('Sita', 32, 'sita123@gmail.com', 100, 50, 30, 'Social')
+let student2 = new Person('Ruja', 30, 'rujasubba123@gmail.com', 100, 50, 88, 'Science')
+   
+console.log(student)
+console.log(student1)
+
+student.isPass()
+student1.isPass()
+student2.isPass()
